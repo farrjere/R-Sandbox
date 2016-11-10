1cccf21ef9ddf8e99808d16342a565aeace7cd36
Jeremy Farrell <farrjere@isu.edu>
2016-11-10 01:39:02 -0700

    minor fix as I am new to R

--- jsonDecisionTrees/R/json_dt.R
+++ jsonDecisionTrees/R/json_dt.R
@@ -1,5 +1,8 @@
 json_dt<-function(data,model){
-  fit<-rpart(data,model)
+  library(rpart)
+  library(rpart.utils)
+  library(jsonlite)
+  fit<-rpart(model,data)
   rules<-rpart.rules.table(fit)
   return (toJSON(rules))
 }
